990eabee04bce6b6a23f11088d41dc4d4bdf0f14
tortious <d_targaryen@mail.com>
2020-07-06 22:16:19 -0400

    Delete date.js

--- date.js
+++
@@ -1,9 +0,0 @@
-function currentDate(){
-	let n =  new Date();
-    let y = n.getFullYear();
-    let m = n.getMonth() + 1;
-    let d = n.getDate();
-    document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
-}
-
-currentDate();
