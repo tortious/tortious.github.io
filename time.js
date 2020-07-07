b7815be833c9382f1e8ab5aa7b45d8130eaaedba
tortious <d_targaryen@mail.com>
2020-07-06 22:16:31 -0400

    Delete time.js

--- time.js
+++
@@ -1,7 +0,0 @@
-window.onload = displayClock();
-function displayClock() {
-	let time = new Date().toLocaleTimeString();
-
-	document.getElementById("clock").innerText = time;
-	setTimeout(displayClock, 1000); /* setting timer */
-}
